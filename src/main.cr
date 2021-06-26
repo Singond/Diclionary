12e746366650803e12b4813f0a6e84b054928246
Jan Singon Slany <singond@seznam.cz>
2021-06-26 16:04:35 +0200

    Change logging setup

--- src/main.cr
+++ src/main.cr
@@ -4,13 +4,20 @@ require "option_parser"
 require "./core.cr"
 require "./ssjc.cr"
 
-logger = ::Log.for("*", :warn)
+# Log.define_formatter Fmt, "#{source}: #{message}"
+Log.define_formatter Fmt, "#{message}"
+Log.setup("*", :warn, Log::IOBackend.new(formatter: Fmt))
+log_level = Log::Severity::Notice
 
 word = ""
 
 parser = OptionParser.new do |p|
-	p.on "-v", "--verbose", "Verbose output" do
-		logger.level = :info
+	p.on "-v", "--verbose", "Increase verbosity" do
+		# Decrease logging level, if possible.
+		lvl = log_level.value
+		if lvl >= 1
+			log_level = Log::Severity.from_value(lvl - 1)
+		end
 	end
 end
 parser.unknown_args do |args|
@@ -19,12 +26,14 @@ parser.unknown_args do |args|
 	end
 end
 parser.parse
+Log.setup("xxx", log_level, Log::IOBackend.new(formatter: Fmt))
+logger = ::Log.for("xxx")
 
 if word.empty?
 	logger.error {"No word given"}
 	exit 1
 else
-	logger.info {"Searching for '#{word}'"}
+	logger.debug {"Searching for '#{word}'"}
 end
 
 dd = [SsjcDictionary.new]
