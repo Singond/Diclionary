214e92cda8f0672e30c92cfdc71d6650c6085ce5
Jan Singon Slany <singond@seznam.cz>
2021-10-12 19:28:09 +0200

    Separate argument parsing from main program

---
+++ src/cli.cr
@@ -0,0 +1,84 @@
+require "option_parser"
+
+require "./main.cr"
+
+module Diclionary::Cli
+	extend self
+
+	private def usage(io : IO, parser : OptionParser)
+		io << parser
+		io << "\n\n"
+		io << "See 'man dicl' for more information."
+		io << "\n"
+	end
+
+	private enum Operation
+		Version
+		Help
+	end
+
+	private def parse_args(args, stdout = STDOUT, stderr = STDERR) \
+			: {Config | Operation, OptionParser}
+		config = Config.new
+		operation = nil
+		parser = OptionParser.new do |p|
+			p.banner = <<-BANNER
+			Usage: dicl [OPTIONS] WORD...
+
+			Options:
+			BANNER
+			p.on "--version", "Show version number and exit" do
+				operation = Operation::Version
+			end
+			p.on "-h", "--help", "Print usage and exit" do
+				operation = Operation::Help
+			end
+			p.on "-v", "--verbose", "Increase verbosity" do
+				# Decrease logging level, if possible.
+				lvl = config.log_level.value
+				if lvl >= 1
+					config.log_level = ::Log::Severity.from_value(lvl - 1)
+				end
+			end
+			p.on "--plain", "Output in plain text format" do
+				config.format = Format::PlainText
+			end
+			p.on "--structured", "Output in structured format" do
+				config.format = Format::Structured
+			end
+		end
+		parser.unknown_args do |args|
+			if args.size > 0
+				config.terms = args
+			end
+		end
+		parser.parse args
+
+		op = operation
+		return {op || config, parser}
+	end
+
+	def run(args = ARGV) : ExitCode
+		begin
+			config, parser = parse_args(args)
+		rescue e : OptionParser::Exception
+			STDERR.puts e.message
+			return ExitCode::BadUsage
+		end
+
+		case config
+		in Operation::Version
+			puts VERSION
+			return ExitCode::Success
+		in Operation::Help
+			usage(STDOUT, parser)
+			return ExitCode::Success
+		in Config
+			if config.terms.empty?
+				usage(STDERR, parser)
+				return ExitCode::BadUsage
+			end
+			Diclionary.run(config)
+		end
+	end
+end
--- src/dicl.cr
+++ src/dicl.cr
@@ -1,8 +1,4 @@
-require "option_parser"
-
-require "./main.cr"
-
-include Diclionary
+require "./cli.cr"
 
 # Setup logging
 Log.define_formatter Fmt, "#{source}: #{message}"
@@ -10,57 +6,6 @@ Log.define_formatter Fmt, "#{source}: #{message}"
 log_backend = Log::IOBackend.new(io: STDERR, formatter: Fmt, dispatcher: :sync)
 Log.setup("*", :warn, log_backend)
 
-def usage(io : IO, parser : OptionParser)
-	io << parser
-	io << "\n\n"
-	io << "See 'man dicl' for more information."
-	io << "\n"
-end
-
-config = Config.new
-parser = OptionParser.new do |p|
-	p.banner = <<-BANNER
-		Usage: dicl [OPTIONS] WORD...
-
-		Options:
-		BANNER
-	p.on "--version", "Show version number and exit" do
-		puts VERSION
-		exit(ExitCode::Success.value)
-	end
-	p.on "-h", "--help", "Print usage and exit" do
-		usage(STDOUT, p)
-		exit(ExitCode::Success.value)
-	end
-	p.on "-v", "--verbose", "Increase verbosity" do
-		# Decrease logging level, if possible.
-		lvl = config.log_level.value
-		if lvl >= 1
-			config.log_level = Log::Severity.from_value(lvl - 1)
-		end
-	end
-	p.on "--plain", "Output in plain text format" do
-		config.format = Format::PlainText
-	end
-	p.on "--structured", "Output in structured format" do
-		config.format = Format::Structured
-	end
-end
-parser.unknown_args do |args|
-	if args.size > 0
-		config.terms = args
-	end
-end
-begin
-	parser.parse
-rescue e : OptionParser::Exception
-	abort(e.message, 2)
-end
-
-if config.terms.empty?
-	usage(STDERR, parser)
-	exit(ExitCode::BadUsage.value)
-end
-
-exit_code = run(config)
-exit exit_code.value
+# Run with CLI arguments
+code = Diclionary::Cli.run
+exit code.value
