b411a98135a60e360925d4db29169cde6a42119d
Jan Singon Slany <singond@seznam.cz>
2021-10-12 20:11:58 +0200

    Rewrite test to use CLI directly

--- spec/cli_spec.cr
+++ spec/cli_spec.cr
@@ -1,12 +1,20 @@
 require "spec"
 
-require "../src/main.cr"
+require "../src/cli.cr"
 
-describe Diclionary do
-	describe "#run" do
-		it "fails if no search terms are given" do
-			config = Diclionary::Config.new
-			exit_code = Diclionary.run(config)
+def run(args = [] of String)
+	stdout = String::Builder.new
+	stderr = String::Builder.new
+	exit_code = Diclionary::Cli.run(args, stdout, stderr)
+	{stdout.to_s, stderr.to_s, exit_code}
+end
+
+describe "#run" do
+	context "without arguments" do
+		it "fails" do
+			output, err, exit_code = run()
+			output.should be_empty
+			err.should_not be_empty
 			exit_code.should eq Diclionary::ExitCode::BadUsage
 		end
 	end
